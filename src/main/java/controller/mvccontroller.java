954d40bc7546ab07be3de7c723f1d4bba4c84425
wenwenwen501 <531689540@qq.com>
2020-11-27 00:18:39 +0800

    MVC

--- src/main/java/controller/mvccontroller.java
+++ src/main/java/controller/mvccontroller.java
@@ -7,18 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import service.Mvcservice;
 
-@Controller
+@RestController(value = "/hello")
 @ResponseBody
 public class mvccontroller {
 
-    @Autowired
-    private Mvcservice mvcservice;
+//    @Autowired
+//    private Mvcservice mvcservice;
 
-    @GetMapping
-    private ResponseVo getRespose(@RequestBody BaseDto baseDto){
-        mvcservice.insert(baseDto);
+//    @GetMapping
+//    private ResponseVo getRespose(@RequestBody BaseDto baseDto){
+//        mvcservice.insert(baseDto);
+//        return new ResponseVo("10000", "success", "httpok");
+//    }
+
+    @GetMapping(value = "/mvc")
+    private ResponseVo hellomvc(){
         return new ResponseVo("10000", "success", "httpok");
     }
 }
