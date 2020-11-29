0cdcd886f9d734b968c4dc4f49a9c9d1bf2c821f
wenwenwen501 <531689540@qq.com>
2020-11-29 22:21:06 +0800

    mvc

--- src/main/java/controller/common/ResponseVo.java
+++ src/main/java/controller/common/ResponseVo.java
@@ -1,6 +1,9 @@
 package controller.common;
 
-public class ResponseVo<T> {
+import java.io.Serializable;
+
+public class ResponseVo<T> implements Serializable{
+    private static final long serialVersionUID = -7454367590246826175L;
     private String code;
     private String msg;
     private T date;
@@ -12,4 +15,28 @@ public class ResponseVo<T> {
 
     }
 
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(final String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return this.msg;
+    }
+
+    public void setMsg(final String msg) {
+        this.msg = msg;
+    }
+
+    public T getDate() {
+        return this.date;
+    }
+
+    public void setDate(final T date) {
+        this.date = date;
+    }
 }
--- src/main/java/controller/mvccontroller.java
+++ src/main/java/controller/mvccontroller.java
@@ -1,11 +1,11 @@
 package controller;
 
 import controller.common.ResponseVo;
-import controller.dto.BaseDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import service.Mvcservice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping(value = "/hello")
