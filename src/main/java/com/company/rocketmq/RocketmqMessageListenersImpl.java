924256d30f81483fe2f4f883a279d3ae639edf6d
Administrator <531689540@qq.com>
2020-12-29 09:19:08 +0800

    consumer_complete

--- src/main/java/com/company/rocketmq/RocketmqMessageListenersImpl.java
+++ src/main/java/com/company/rocketmq/RocketmqMessageListenersImpl.java
@@ -1,14 +1,7 @@
 package com.company.rocketmq;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.common.message.MessageExt;
-import org.springframework.beans.factory.annotation.Value;
-
-import java.util.List;
 
 public class RocketmqMessageListenersImpl extends DefaultMQPushConsumer {
 
